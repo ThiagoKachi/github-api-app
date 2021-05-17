62ef7ca38c25a67d1f81979339d8697d311a0e3d
Thiago Kachinsky <tjk091@gmail.com>
2021-05-17 20:21:40 -0300

    Criando a marcação inicial da aplicação

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
 import React from 'react';
+import Actions from './components/Actions';
+import Repos from './components/Repos';
+import Search from './components/Search';
+import UserInfo from './components/UserInfo';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="se">
-          <input 
-            type="search" 
-            name="" 
-            placeholder="Digite o nome do usuários do Github" 
-          />
-        </div>
-
-        <div className="userInfo">
-          <img src="https://avatars.githubusercontent.com/u/61670495?v=4" alt="Thiago" />
-          <h1>
-            <a href="https://api.github.com/users/ThiagoKachi">Thiago Kachinsky</a> 
-          </h1>
-
-          <ul className="repos-info">
-            <li>Repositórios: 16</li>
-            <li>Seguidores: 17</li>
-            <li>Seguindo: 12</li>
-          </ul>
-
-          <div className="actions">
-            <button>Ver Repositórios</button>
-            <button>Ver Favoritos</button>
-          </div>
-
-          <div className="repos">
-            <h2>Repositórios</h2>
-            <ul>
-              <li> <a href="#">Nome do repositório</a> </li>
-            </ul>
-          </div>
-
-          <div className="starred">
-            <h2>Favoritos</h2>
-            <ul>
-              <li> <a href='#'>Nome do repositório</a> </li>
-            </ul>
-          </div>
-        </div>
+        <Search />
+        <UserInfo />
+        <Actions />
+        <Repos 
+          className='repos' 
+          title='Repositórios' 
+          repos={[{
+            name: 'Nome do repositório',
+            link: '#'
+          }]}
+        />
+        <Repos 
+          className='starred' 
+          title='Favoritos' 
+          repos={[{
+            name: 'Nome do repositório',
+            link: '#'
+          }]}
+        />
       </div>
     )
   }
